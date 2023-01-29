df4fb6df29f145ea90b658b492ff86a3e803c758
youngmentor <olaleresuliton43@gmail.com>
2023-01-29 08:34:23 -0800

    refactor the why page

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import Hero from './Components/Hero/Hero'
 import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom'
 import { ThemeContext } from './Components/ContextApi/Context'
 import Login from './Components/Login/Login'
+import './App.css'
 const App = () => {
-    const {removeHeader, header} = useContext(ThemeContext)
-    
   return (
-    <div>
+
+    <div className='app'>
       <HashRouter>
-      <Header />
+         <Header />
          <Routes>
             <Route path="/" element={<Hero/>}/>
             <Route path="signup" element={<Form/>} />
