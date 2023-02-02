10a88b1844f04c484c0a1340ac1a785baaa77c58
youngmentor <olaleresuliton43@gmail.com>
2023-02-02 11:24:41 -0800

    changes

--- src/Components/Hero/HeroFeatures.jsx
+++ src/Components/Hero/HeroFeatures.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Datas}  from './Data'
+import { Datas } from './Data'
 import './HeroFeatures.css'
 const HeroFeatures = () => {
- const navigate = useNavigate()
-  const style ={
+  const navigate = useNavigate()
+  const style = {
     flexDirection: "row-reverse",
     backgroundColor: "blue"
   }
@@ -13,22 +13,22 @@ const HeroFeatures = () => {
     <div className='MainFeatures'>
       <h1 className='FeaturesHeader' >Features Of Edu-Global</h1>
       <div className='Features'>
-      
-       {Datas?.map((i)=>
+
+        {Datas?.map((i) =>
           <div className='Features-Wrapper'>
-          <div className='Features-text'>
-            <div className='Heading'><h1>{i.bgtxt}</h1></div>
-            <div className='Admin-img' >
-            <img src={i.Img} className="FeaturesImg" />
+            <div className='Features-text'>
+              <div className='Heading'><h1>{i.bgtxt}</h1></div>
+              <div className='Admin-img' >
+                <img src={i.Img} className="FeaturesImg" />
+              </div>
+              <div className='paragraph'><p>{i.smtxt}</p></div>
+            </div>
           </div>
-            <div className='paragraph'><p>{i.smtxt}</p></div>
-          </div>
-        </div>
-       )}
-    </div>
-    <div className='HeaderbttnDiv'>
-           <button className='Herobtt' onClick={() => navigate("signUp")} >Sign Up here</button>
-           </div>
+        )}
+      </div>
+      <div className='HeaderbttnDiv'>
+        <button className='Herobtt' onClick={() => navigate("signUp")} >Sign Up here</button>
+      </div>
     </div>
   )
 }
