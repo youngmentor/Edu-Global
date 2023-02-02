023c4756a5008e3fbad0455a097e290cd10f958b
youngmentor <olaleresuliton43@gmail.com>
2023-02-02 07:33:54 -0800

    changes

--- src/Components/AdminDashboard/AdminLeft.jsx
+++ src/Components/AdminDashboard/AdminLeft.jsx
@@ -4,21 +4,25 @@ import './Admin.css'
 const AdminLeft = (props) => {
     const [toggle, setToggle] = useState(false)
     return (
-        <div >       
-                <div className='AdminSidebar'>
-                    <div className='AdminNav'>
-                       <div> {props.icon1}</div>
-                        <h3>{props.title}</h3>
-                        {toggle ? <div onClick={() => { setToggle(!toggle) }} className="AdminLeftArrowIcon" >{props.icon3}</div> : <div onClick={() => { setToggle(!toggle) }} className="AdminLeftArrowIcon"  >{props.icon2}</div>}
-
-                    </div>
-                    {toggle && <div className='DashDrop'>
-                        <p>{props.text1}</p>
-                        {props.text2 && <p>{props.text2}</p>}
-                        {props.text3 && <p>{props.text3}</p>}
-                        {props.text4 && <p>{props.text4}</p>}
-                    </div>}
+        <div >
+            <div className='AdminSidebar'>
+                <div className='AdminNav'>
+                    <div> {props.icon1}</div>
+                    <h3>{props.title}</h3>
+                    {toggle ? <div onClick={() => { setToggle(!toggle) }} className="AdminLeftArrowIcon" >
+                        {props.icon3}
+                    </div> :
+                        <div onClick={() => { setToggle(!toggle) }} className="AdminLeftArrowIcon"  >
+                            {props.icon2}
+                        </div>}
                 </div>
+                {toggle && <div className='DashDrop'>
+                    <p>{props.text1}</p>
+                    {props.text2 && <p>{props.text2}</p>}
+                    {props.text3 && <p>{props.text3}</p>}
+                    {props.text4 && <p>{props.text4}</p>}
+                </div>}
+            </div>
         </div>
     )
 }
