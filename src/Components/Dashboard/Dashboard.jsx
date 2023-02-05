d3ec7eb3f956f03eadd6c38bb039b89943a9a3a3
youngmentor <olaleresuliton43@gmail.com>
2023-02-04 19:42:08 -0800

    new commit

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import './dashboard.css'
 import Calendar from 'react-calendar'
 import React, { useState } from 'react'
-
+import { FaGraduationCap } from "react-icons/fa";
 
 export default function Dashboard() {
     const [date, setDate] = useState(new Date());
@@ -19,16 +19,52 @@ export default function Dashboard() {
             </div>
             <div className='AdminCardHolder'>
                 <div className='AdminCard'>
-                    <h1>Cards</h1>
+                  <div className='AdminCardWrap'>
+                  <div className='AdminCardDetails'>
+                        <h4>Jss1</h4>
+                        <h2>1</h2>
+                        <h4>Students</h4>
+                    </div>
+                    <div className='AdminGraduationCap'>
+                      <FaGraduationCap style={{ width: "50px", height: "50px" }}  />  
+                    </div>  
+                  </div>
                 </div>
                 <div className='AdminCard'>
-                    <h1>Cards</h1>
+                <div className='AdminCardWrap'>
+                  <div className='AdminCardDetails'>
+                        <h4>Jss1</h4>
+                        <h2>1</h2>
+                        <h4>Students</h4>
+                    </div>
+                    <div className='AdminGraduationCap'>
+                      <FaGraduationCap style={{ width: "50px", height: "50px" }}  />  
+                    </div>  
+                  </div>
                 </div>
                 <div className='AdminCard'>
-                    <h1>Cards</h1>
+                <div className='AdminCardWrap'>
+                  <div className='AdminCardDetails'>
+                        <h4>Jss1</h4>
+                        <h2>1</h2>
+                        <h4>Students</h4>
+                    </div>
+                    <div className='AdminGraduationCap'>
+                      <FaGraduationCap style={{ width: "50px", height: "50px" }}  />  
+                    </div>  
+                  </div>
                 </div>
                 <div className='AdminCard'>
-                    <h1>Cards</h1>
+                <div className='AdminCardWrap'>
+                  <div className='AdminCardDetails'>
+                        <h4>Jss1</h4>
+                        <h2>1</h2>
+                        <h4>Students</h4>
+                    </div>
+                    <div className='AdminGraduationCap'>
+                      <FaGraduationCap style={{ width: "50px", height: "50px" }}  />  
+                    </div>  
+                  </div>
                 </div>
             </div>
             <Calendar onChange={handleChange} value={date} className="Calender" />
