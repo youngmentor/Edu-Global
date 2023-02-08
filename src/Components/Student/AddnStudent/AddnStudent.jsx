647b336b1f9504dd27b7447b2e254120536b7621
youngmentor <olaleresuliton43@gmail.com>
2023-02-07 19:33:02 -0800

    new

--- src/Components/Student/AddnStudent/AddnStudent.jsx
+++ src/Components/Student/AddnStudent/AddnStudent.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import './AddnStudent.css'
 const AddnStudent = () => {
   return (
-    <div>
-        welcome new student
+    <div className='AdminStudents_All'>
+       <div>
+        <div>
+          <h1>welcome</h1>
+        </div>
+       </div>
     </div>
   )
 }
