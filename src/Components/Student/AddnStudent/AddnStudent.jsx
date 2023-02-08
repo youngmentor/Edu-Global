dbf7f71c286e3874709f42c1cfb62078d2c0e8eb
youngmentor <olaleresuliton43@gmail.com>
2023-02-07 20:41:21 -0800

    new

--- src/Components/Student/AddnStudent/AddnStudent.jsx
+++ src/Components/Student/AddnStudent/AddnStudent.jsx
@@ -3,9 +3,73 @@ import './AddnStudent.css'
 const AddnStudent = () => {
   return (
     <div className='AdminStudents_All'>
-     <div>
-      welcome
-     </div>
+      <div className='AdminStudents_All_Wrap'>
+       <div className='AdminStudent_Img'> 
+       <img src="/UserImg.png" alt="StudentLogo" className='AdminStudent_Avatar' />
+       </div>
+       <div className='AdminStudent_Details'>
+        <p>111009</p>
+        <p>suliton</p>
+       </div>
+      </div>
+      <div className='AdminStudents_All_Wrap'>
+       <div className='AdminStudent_Img'> 
+       <img src="/UserImg.png" alt="StudentLogo" className='AdminStudent_Avatar' />
+       </div>
+       <div className='AdminStudent_Details'>
+        <p>111009</p>
+        <p>suliton</p>
+       </div>
+      </div>
+      <div className='AdminStudents_All_Wrap'>
+       <div className='AdminStudent_Img'> 
+       <img src="/UserImg.png" alt="StudentLogo" className='AdminStudent_Avatar' />
+       </div>
+       <div className='AdminStudent_Details'>
+        <p>111009</p>
+        <p>suliton</p>
+       </div>
+      </div>
+      <div className='AdminStudents_All_Wrap'>
+       <div className='AdminStudent_Img'> 
+       <img src="/UserImg.png" alt="StudentLogo" className='AdminStudent_Avatar' />
+       </div>
+       <div className='AdminStudent_Details'>
+        <p>111009</p>
+        <p>suliton</p>
+       </div>
+      </div>
+      <div className='AdminStudents_All_Wrap'>
+       <div className='AdminStudent_Img'> 
+       <img src="/UserImg.png" alt="StudentLogo" className='AdminStudent_Avatar' />
+       </div>
+       <div className='AdminStudent_Details'>
+        <p>111009</p>
+        <p>suliton</p>
+       </div>
+      </div>
+      <div className='AdminStudents_All_Wrap'>
+       <div className='AdminStudent_Img'> 
+       <img src="/UserImg.png" alt="StudentLogo" className='AdminStudent_Avatar' />
+       </div>
+       <div className='AdminStudent_Details'>
+        <p>111009</p>
+        <p>suliton</p>
+       </div>
+      </div>
+      <div className='AdminStudents_All_Wrap'>
+       <div className='AdminStudent_Img'> 
+       <img src="/UserImg.png" alt="StudentLogo" className='AdminStudent_Avatar' />
+       </div>
+       <div className='AdminStudent_Details'>
+        <p>111009</p>
+        <p>suliton</p>
+       </div>
+      </div>
+      <div className='AdminStudents_All_AddWrap'>
+           <p>+</p>
+           <p>Add New</p>
+      </div>
     </div>
   )
 }
