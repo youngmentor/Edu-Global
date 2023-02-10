cff3ad0c5cc85e4b0ea83ce94597288cdfeddb2d
youngmentor <olaleresuliton43@gmail.com>
2023-02-10 00:04:50 -0800

    new commit

--- src/Components/Teachers/AddTeacher/AddTeacher.jsx
+++ src/Components/Teachers/AddTeacher/AddTeacher.jsx
@@ -1,9 +1,106 @@
 import React from 'react'
 import './AddTeacher.css'
+import { useState } from 'react'
 const AddTeacher = () => {
+  const [AddnTeacher, SetAddnTeacher]= useState()
+  const AddTeacher = [
+    { 
+      id: 1,
+        name: "name",
+        label: "name",
+        placeholder: "Name of Student",
+        type: "text"
+    },
+    {
+      id: 2,
+        name: "numbers",
+        label: "number",
+        placeholder: "Mobile No",
+        type: "number"
+    },
+    {
+      id: 3,
+        name: "number",
+        label: "number",
+        placeholder: "Student/Guardian No:",
+        type: "number"
+    },
+    {
+      id: 4,
+        name: "Picture",
+        label: "file",
+        placeholder: "Choose A file",
+        type: "file"
+    },
+    {
+      id: 5,
+        name: "address",
+        label: "address",
+        placeholder: "Home Address:",
+        type: "select"
+    },
+    {
+      id: 6,
+        name: "date",
+        label: "date",
+        placeholder: "joining date",
+        type: "date"
+    },
+    {
+      id: 7,
+        name: "birth",
+        label: "birth",
+        placeholder: "Date Of Birth",
+        type: "date"
+    },
+    {
+      id: 8,
+        name: "education",
+        label: "education",
+        placeholder: "Level Of Education",
+        type: "text"
+    },
+    {
+      id: 9,
+        name: "religion",
+        label: "religion",
+        placeholder: "Religion",
+        type: "text"
+    },
+    {
+      id: 10,
+        name: "gender",
+        label: "gender",
+        placeholder: "Gender",
+        type: "text"
+    },
+];
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  SetAddnTeacher({ ...AddnTeacher, [name]: value });
+};
+const handleSubmit = (e) => {
+  e.preventDefault();
+};
   return (
-    <div>
-        good morning teachers
+    <div className='AdminAddn_Teacher'>
+         <div className='AdminAddn_TeacherWrap'>
+       <form onSubmit={handleSubmit} className="AddTeacher-inputWrap" >
+            {AddTeacher.map((i) => (
+                        <label className="AddTeacher_Label"  key={i.label}  >
+                            <input
+                              label={i.label}
+                                className="AddTeacher_Input"
+                                type={i.type}
+                                name={i.name}
+                                placeholder={i.placeholder}
+                                onChange={handleChange}
+                            />
+                        </label>
+                    ))}
+                      <button type="submit" className="Addbttn">Submit form</button>
+            </form>
+       </div>
     </div>
   )
 }
--- src/Components/Teachers/AllTeachers/AllTeachers.jsx
+++ src/Components/Teachers/AllTeachers/AllTeachers.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './AllTeachers.css'
+import { NavLink } from 'react-router-dom'
 const AllTeachers = () => {
   return (
     <div className='AdminTeacher_All'>
@@ -8,11 +9,26 @@ const AllTeachers = () => {
        <img src="/UserImg.png" alt="TeacherLogo" className='AdminTeacher_Avatar' />
        </div>
        <div className='AdminTeacher_Details'>
-        <p>1</p>
         <p>Anthony</p>
        </div>
       </div>
-      <NavLink className='AdminStudents_All_AddWrap' to="/admin/student/updatestudents" >
+      <div className='AdminTeachers_All_Wrap'>
+       <div className='AdminTeachers_Img'> 
+       <img src="/UserImg.png" alt="TeacherLogo" className='AdminTeacher_Avatar' />
+       </div>
+       <div className='AdminTeacher_Details'>
+        <p>Favour</p>
+       </div>
+      </div>
+      <div className='AdminTeachers_All_Wrap'>
+       <div className='AdminTeachers_Img'> 
+       <img src="/UserImg.png" alt="TeacherLogo" className='AdminTeacher_Avatar' />
+       </div>
+       <div className='AdminTeacher_Details'>
+        <p>Edidiong</p>
+       </div>
+      </div>
+      <NavLink className='AdminTeacher_All_AddWrap' to="/admin/teachers/addteacher" >
            <p>+</p>
            <p>Add New</p>
       </NavLink>
