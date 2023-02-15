dfedaa827d36f258d2812731afc1e1c85508cd33
youngmentor <olaleresuliton43@gmail.com>
2023-02-14 23:48:14 -0800

    new commit

--- src/Components/Attendance/Attendance.jsx
+++ src/Components/Attendance/Attendance.jsx
@@ -1,7 +1,7 @@
 import './attendance.css'
 import { Route, Routes } from 'react-router-dom'
 import React from 'react'
-import StudentAttendance from './StudentAttendance/StudentAttendance'
+// import StudentAttendance from './StudentAttendance/StudentAttendance'
 import TeacherAttendance from './TeacherAttendance/TeacherAttendance'
 
 export default function Attendance() {
@@ -10,7 +10,7 @@ export default function Attendance() {
     <div className='Admin_Attendance'>
       <div className='AdminAttendanceWrap'>
         <Routes>
-          <Route path='studentattendance' element={<StudentAttendance/>} />
+          {/* <Route path='studentattendance' element={<StudentAttendance/>} /> */}
           <Route path='teacherattendance' element={<TeacherAttendance/>} />
         </Routes>
       </div>
