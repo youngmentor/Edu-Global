ec9fe0f945848fe97cdef55c357768b1a36c142a
youngmentor <olaleresuliton43@gmail.com>
2023-02-17 14:42:05 -0800

    adde react icon

--- src/Component2/StudentDashboard/StudentDashLeft.jsx
+++ src/Component2/StudentDashboard/StudentDashLeft.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react'
 // import { GiCondorEmblem } from 'react-icons/gi'
 import { useNavigate, Link } from 'react-router-dom'
+import { RxDashboard } from "react-icons/rx";
+import { BsListTask} from "react-icons/bs";
+import { HiOutlineDocumentReport} from "react-icons/hi";
+import { MdOutlinePayments} from "react-icons/md";
+import { AiOutlineSetting } from "react-icons/ai";
 import "./StudentDash.css"
 const StudentDashLeft = () => {
     const navigate = useNavigate()
     const [toggle, setToggle] = useState(false)
     return (
         <div className='StudentNav'>
-            <Link to={"/studentdash"} className="Dash" ><h3>Dashboard</h3></Link>
-            <Link to={"table"} className="Dash" ><h3>TimeTable</h3></Link>
-            <Link to={"report"} className="Dash"><h3>Report Card</h3></Link>
-            <Link to={"payfee"} className="Dash"><h3>Pay Fee</h3></Link>
-            <Link to={"accountset"} className="Dash"><h3>Account Setting</h3></Link>
-            <h3>Logout</h3>
+            <Link to={"/studentdash"} className="Dash" ><h3><RxDashboard/> Dashboard</h3></Link>
+            <Link to={"table"} className="Dash" ><h3> <BsListTask/> TimeTable</h3></Link>
+            <Link to={"report"} className="Dash"><h3> <HiOutlineDocumentReport/>Report Card</h3></Link>
+            <Link to={"payfee"} className="Dash"><h3> <MdOutlinePayments/> Pay Fee</h3></Link>
+            <Link to={"accountset"} className="Dash"><h3> <AiOutlineSetting/> Account Setting</h3></Link>
+            <Link to={"logout"} className="Dash"> <h3>Logout</h3></Link>
         </div>
     )
 }
