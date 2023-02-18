cfb5ce51cd0bbdf7a9b5313a23e660b653017de0
youngmentor <olaleresuliton43@gmail.com>
2023-02-18 07:51:29 -0800

    new component created

--- src/Component2/StudentDashboard/StudentDashLeft.jsx
+++ src/Component2/StudentDashboard/StudentDashLeft.jsx
@@ -6,6 +6,7 @@ import { BsListTask} from "react-icons/bs";
 import { HiOutlineDocumentReport} from "react-icons/hi";
 import { MdOutlinePayments} from "react-icons/md";
 import { AiOutlineSetting } from "react-icons/ai";
+import { BiLogOut } from "react-icons/bi";
 import "./StudentDash.css"
 const StudentDashLeft = () => {
     const navigate = useNavigate()
@@ -17,7 +18,7 @@ const StudentDashLeft = () => {
             <Link to={"report"} className="Dash"><h3> <HiOutlineDocumentReport/>Report Card</h3></Link>
             <Link to={"payfee"} className="Dash"><h3> <MdOutlinePayments/> Pay Fee</h3></Link>
             <Link to={"accountset"} className="Dash"><h3> <AiOutlineSetting/> Account Setting</h3></Link>
-            <Link to={"logout"} className="Dash"> <h3>Logout</h3></Link>
+            <Link to={"logout"} className="Dash"> <h3> <BiLogOut/> Logout</h3></Link>
         </div>
     )
 }
---
+++ src/Component3/AccountSetting_Teach/AccountSetting_Teach.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AccountSetting_Stu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AccountSetting_Stu
