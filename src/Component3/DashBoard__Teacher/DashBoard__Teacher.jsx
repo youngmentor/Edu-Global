c22b8b9169817d18ece9c73baa7841862a2f07d5
youngmentor <olaleresuliton43@gmail.com>
2023-02-19 06:19:36 -0800

    new changes

--- src/Component3/DashBoard__Teacher/DashBoard__Teacher.jsx
+++ src/Component3/DashBoard__Teacher/DashBoard__Teacher.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "./DashBoard__Teacher.css"
+import Calendar from 'react-calendar'
+const Dashboard_Teacher= () => {
 
-const DashBoard__Teacher = () => {
+  const [date, setDate] = useState(new Date());
+
+  const handleChange = (selectedDate) => {
+      setDate(selectedDate);
+  };
   return (
-    <div>DashBoard__Teacher</div>
+    <div className='Dashboard_Stu_Main'>
+      <div className='Dashboard_Stu_Main_Wrap' >
+        <div className='Dashboard_Stu_Info_Right' >
+          <div className='Dashboard_Stu_Info_Detail'>
+            <img src='/UserImg.png' alt='' className='Dashboard_Stu_Info_Img' />
+            <h3>Mr Anthony</h3>
+          </div>
+          <div className='Dashboard_info'>
+             <div className='Dashboard_detail'>
+               <h5>Reg No:</h5>
+               <h5>001</h5>
+             </div>
+             <div className='Dashboard_detail'>
+               <h5>Date of Admission:</h5>
+               <h5>2008-10-15</h5>
+             </div>
+             <div className='Dashboard_detail'>
+               <h5>Class:</h5>
+               <h5>ss3</h5>
+             </div>
+             <div className='Dashboard_detail'>
+               <h5>Date of Birth:</h5>
+               <h5>001</h5>
+             </div>
+             <div className='Dashboard_detail'>
+               <h5>Gender:</h5>
+               <h5>Male</h5>
+             </div>
+          </div>
+        </div>
+      <div className='Dashboard_Stu_Left'>
+      <div className='Dashboard_Stu_Msg' >
+          <h4>Mr Suliton Welcome to student portal<br/> you have access to your info</h4>
+          <img src='/Message.png' alt='' className='Dashboard_Stu_Info_Mss' />
+        </div>
+        <Calendar onChange={handleChange} value={date} className="Calender" />
+      </div>
+      </div>
+    </div>
   )
 }
 
-export default DashBoard__Teacher
+export default Dashboard_Teacher
