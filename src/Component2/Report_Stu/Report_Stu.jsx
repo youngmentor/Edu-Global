1f5f16bbbc6470cb013a4ced0c70c7de74cc674e
youngmentor <olaleresuliton43@gmail.com>
2023-02-18 18:07:57 -0800

    install a file saver package

--- src/Component2/Report_Stu/Report_Stu.jsx
+++ src/Component2/Report_Stu/Report_Stu.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import FileSaver from 'file-saver';
+import FileSaver from 'file-saver';
 import "./Report_Stu.css"
 function Report_Stu({ studentId }) {
   const [studentName, setStudentName] = useState('');
--- src/Component2/Timetable_Stu/Timetable_Stu.jsx
+++ src/Component2/Timetable_Stu/Timetable_Stu.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import FileSaver from 'file-saver';
+import FileSaver from 'file-saver';
 
 function Timetabl_Stu({ studentId }) {
   const [studentName, setStudentName] = useState('');
