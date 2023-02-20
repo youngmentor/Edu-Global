5841690586745263feeee5129ffd24ced2e285e5
youngmentor <olaleresuliton43@gmail.com>
2023-02-20 15:18:44 -0800

    latest commit

--- src/Components/ForgetPassword/ForgetPassword.jsx
+++ src/Components/ForgetPassword/ForgetPassword.jsx
@@ -6,10 +6,28 @@ import { useNavigate } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 // import axios from 'axios';
 const ForgetPassword = () => {
-    const inputRef = useRef('')
+    // const inputRef = useRef('')
 
     const navigate = useNavigate()
-    // const forget = () => {
+  
+
+    return (
+        <div className='forget'>
+            <div className='forget_text'>
+                <h2>Forgot password?</h2>
+                {/* <p>No worries, we’ll send you reset instructions</p> */}
+            </div>
+            <input type="email" className='forget_input' placeholder="Please Enter your email..." />
+            <button className='forget_button pointer' onClick={() => { navigate('/resetpassword') }}  >Reset password</button>
+            {/* <div className='forget_back pointer' onClick={() => { navigate('/loginuser/login') }} ><MdOutlineArrowBack fontSize={20} /><p>Back to login</p></div> */}
+        </div>
+    )
+}
+
+export default ForgetPassword
+// onClick={() => { forget() }}
+
+  // const forget = () => {
     //     const Email = { email: inputRef.current.value }
     //     console.log(Email.email)
     //     axios.post("", Email)
@@ -19,19 +37,3 @@ const ForgetPassword = () => {
     //         console.log(e)
     //       })
     //   }
-
-    return (
-        <div className='forget'>
-            <div className='forget_text'>
-                <h2>Forgot password?</h2>
-                <p>No worries, we’ll send you reset instructions</p>
-            </div>
-            <input type="email" className='forget_input' ref={inputRef} placeholder="Please Enter your email..." />
-            <button className='forget_button pointer' onClick={() => { navigate('/resetpassword') }}  >Reset password</button>
-            <div className='forget_back pointer' onClick={() => { navigate('/loginuser/login') }} ><MdOutlineArrowBack fontSize={20} /><p>Back to login</p></div>
-        </div>
-    )
-}
-
-export default ForgetPassword
-// onClick={() => { forget() }}
