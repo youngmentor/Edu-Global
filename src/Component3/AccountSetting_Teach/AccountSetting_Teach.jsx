d99ca3c11b0a1e292adfd7e6e18824205451a2b1
youngmentor <olaleresuliton43@gmail.com>
2023-02-21 23:45:20 -0800

    new commit

--- src/Component3/AccountSetting_Teach/AccountSetting_Teach.jsx
+++ src/Component3/AccountSetting_Teach/AccountSetting_Teach.jsx
@@ -2,17 +2,26 @@ import React from 'react'
 import "./AccountSetting_Teach.css"
 import { useRef } from 'react'
 const AccountSetting_Teach = () => {
-  const inputRef=useRef()
+  const inputRef = useRef()
   return (
     <div className='UpdateAccountTeach_Main'>
-       <div className='accountsetteach_text'>
-                <h4>Please Enter a New password and Email</h4>
-            </div>
-            <input type="text" className='UpdateAccountTeach' ref={inputRef} placeholder="Enter your new Email..." />
-            <input type="passowrd" className='UpdateAccountTeach' ref={inputRef} placeholder="Enter your new PassWord..." />
-            <button className='UpdateAccounTeacht_Bttn' >Update</button>
+      <div className='UpdateAccountTeach_Main_Wrap'>
+        <div className='UpdateAccountTeach_Main_Wrap1'>
+          <div className='accountsetteach_text'>
+            <h4>Please Enter a New password and Email</h4>
+          </div>
+          <input type="text" className='UpdateAccountTeach' ref={inputRef} placeholder="Enter your new Email..." />
+          <input type="passowrd" className='UpdateAccountTeach' ref={inputRef} placeholder="Enter your new PassWord..." />
+          <button className='UpdateAccounTeacht_Bttn' >Update</button>
+        </div>
+        <div className='UpdateAccountTeach_Main_Wrap2' >
+          <h4>Account Login Details</h4>
+          <div className='UpdateAccountTeach' >Email:<p></p> </div>
+          <div className='UpdateAccountTeach' >PassWord: <p></p></div>
+          <button className='UpdateAccounTeacht_Bttn2' >Delete Account</button>
+        </div>
+      </div>
     </div>
   )
 }
-
 export default AccountSetting_Teach
--- src/Components/AccountSetting/AccountSetting.jsx
+++ src/Components/AccountSetting/AccountSetting.jsx
@@ -4,13 +4,23 @@ const AccountSetting = () => {
   const inputRef = useRef('')
 
   return (
-    <div className='UpdateAccount_Main'>
-         <div className='forget_text'>
-                <h2>Please Enter a New password</h2>
-            </div>
-            <input type="text" className='UpdateAccount' ref={inputRef} placeholder="Enter your new Email..." />
-            <input type="passowrd" className='UpdateAccount' ref={inputRef} placeholder="Enter your new PassWord..." />
-            <button className='UpdateAccount_Bttn' >Update</button>
+    <div className='UpdateAccountAdmin_Main'>
+      <div className='UpdateAccountAdmin_Main_Wrap'>
+        <div className='UpdateAccountAdmin_Main_Wrap1'>
+          <div className='accountsetAdmin_text'>
+            <h4>Please Enter a New password and Email</h4>
+          </div>
+          <input type="text" className='UpdateAccountAdmin' ref={inputRef} placeholder="Enter your new Email..." />
+          <input type="passowrd" className='UpdateAccountAdmin' ref={inputRef} placeholder="Enter your new PassWord..." />
+          <button className='UpdateAccounAdmint_Bttn' >Update</button>
+        </div>
+        <div className='UpdateAccountAdmin_Main_Wrap2' >
+          <h4>Account Login Details</h4>
+          <div className='UpdateAccountAdmin' >Email:<p></p> </div>
+          <div className='UpdateAccountAdmin' >PassWord: <p></p></div>
+          <button className='UpdateAccounAdmint_Bttn2' >Delete Account</button>
+        </div>
+      </div>
     </div>
   )
 }
