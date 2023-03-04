f0b4ce4976a36c90d6948269b3774edc9315b2e2
youngmentor <olaleresuliton43@gmail.com>
2023-03-04 11:23:21 -0800

    new changes

--- src/Components/Logins/LoginTeacher/LoginTeacher.jsx
+++ src/Components/Logins/LoginTeacher/LoginTeacher.jsx
@@ -3,17 +3,18 @@ import { useNavigate } from "react-router-dom";
 import './Login.css'
 import LoginUser from "../LoginUser";
 import Loading from "../../LoadingSpin/Loading";
+import { addTeacher } from "../../../Redux/Features";
 import axios from "axios";
 const LoginTeacher = () => {
     const [load, setLoad] = useState(false)
     const navigate = useNavigate()
-     const [value, setValue] = useState({
+    const [value, setValue] = useState({
         email: "",
         password: ""
     })
     const field = [
         {
-            name: "Email",
+            name: "email",
             label: "Email",
             placeholder: "Email",
             type: "text"
@@ -29,36 +30,36 @@ const LoginTeacher = () => {
     const handleChange = (event) => {
         setValue({ ...value, [event.target.name]: event.target.value })
     };
-   const handleSubmit =  (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         console.log("clicked")
         setLoad(true)
-     axios.post(`https://eduglobal.onrender.com/api/teacher/login`, value)
-        .then(function(res){
-            localStorage.setItem("res", JSON.stringify(res));
-            console.log(res.data)
+        axios.post(`https://eduglobal.onrender.com/api/teacher/login`, value)
+            .then(function (res) {
+                localStorage.setItem("res", JSON.stringify(res));
+                console.log(res.data)
                 console.log(res.data.message)
-                res.data.data.email === value.email ? dispatch(addUser(res.data.data)) : null
+                res.data.data.email === value.email ? dispatch(addTeacher(res.data.data)) : null
                 res.data.data.email === value.email ? navigate('/teacherdash') : null
-        })
-        .catch(function(error){
-            console.log(error);
-            setLoad(false)
-        })
+            })
+            .catch(function (error) {
+                console.log(error);
+                setLoad(false)
+            })
     };
 
     return (
         <main className="Login" >
             <form onSubmit={handleSubmit} className="login-wrap" >
                 <div className="LoginForm">
-                < div className="UserLogin">
+                    < div className="UserLogin">
                         <div className="Teacher" onClick={() => navigate("/loginuser/loginteacher")}>Teacher</div>
                         <div className="Admin" onClick={() => navigate("/loginuser/login")}>Admin</div>
                         <div className="Students" onClick={() => navigate("/loginuser/loginstudent")}   >Student</div>
                     </div>
                     <div className="LoginType">
-                   <p>I'M a Teacher</p>
-                   </div>
+                        <p>I'M a Teacher</p>
+                    </div>
                     {field.map((i) => (
                         <label key={i.name}>
                             <input
