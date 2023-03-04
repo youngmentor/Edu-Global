8e27bda0954304af7456a15a6d33b1f547a749d2
youngmentor <olaleresuliton43@gmail.com>
2023-03-04 11:47:16 -0800

    new commit

--- src/Component2/Dashboard_Stu/Dashboard_Stu.jsx
+++ src/Component2/Dashboard_Stu/Dashboard_Stu.jsx
@@ -1,44 +1,63 @@
 import React, { useState } from 'react'
 import "./Dashboard_Stu.css"
 import Calendar from 'react-calendar'
+import axios from 'axios';
+import { addStudent } from '../../Redux/Features';
+import { useDispatch, useSelector } from "react-redux";
 const Dashboard_Stu = () => {
-
+const [oneStudent, setOneStudent]=useState([])
   const [date, setDate] = useState(new Date());
-
+  const student = useSelector((state) => state.Commerce.student)
   const handleChange = (selectedDate) => {
     setDate(selectedDate);
   };
+
+  const getStudent = () =>{
+    axios.get(`https://eduglobal.onrender.com/api/admin/Student/${student?._id}`)
+    setOneStudent(res.data.data)
+  }
+
+  useEffect(() => {
+    getStudent()
+
+  }, [])
+  useEffect(() => {
+    console.log(res)
+    // console.log(Allclass)
+  }, [oneStudent])
   return (
     <div className='Dashboard_Students_Main'>
       <div className='Dashboard_Students_Main_Wrap' >
-        <div className='Dashboard_Students_Info_Right' >
+      {oneStudent?.map((i)=>(
+          <div className='Dashboard_Students_Info_Right' >
           <div className='Dashboard_Students_Info_Detail'>
             <img src='/UserImg.png' alt='' className='Dashboard_Students_Info_Img' />
-            <h3>Suliton</h3>
+            <h3>{i.studentName}</h3>
           </div>
           <div className='Dashboard_Students_info'>
             <div className='Dashboard_Students_detail'>
               <h5>Reg No:</h5>
-              <h5>001</h5>
+              <h5>{i.regNumbe}</h5>
             </div>
             <div className='Dashboard_Students_detail'>
               <h5>Date of Admission:</h5>
-              <h5>2008-10-15</h5>
+              <h5> {i.admissionYear} </h5>
             </div>
             <div className='Dashboard_Students_detail'>
               <h5>Class:</h5>
-              <h5>ss3</h5>
+              <h5> {i.nameOfClass} {i.classBranch} </h5>
             </div>
             <div className='Dashboard_Students_detail'>
               <h5>Date of Birth:</h5>
-              <h5>001</h5>
+              <h5>{i.DOB}</h5>
             </div>
             <div className='Dashboard_Students_detail'>
               <h5>Gender:</h5>
-              <h5>Male</h5>
+              <h5>{i.gender}</h5>
             </div>
           </div>
         </div>
+      ))}
         <div className='Dashboard_Students_Left'>
           <div className='Dashboard_Students_Msg' >
             <h4>Suliton Welcome to student portal<br /> you have access to your info</h4>
