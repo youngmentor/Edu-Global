777b7e7bcfc418207cfc6d59f954ddaf29481d0c
youngmentor <olaleresuliton43@gmail.com>
2023-03-06 14:01:05 -0800

    new commit

--- src/Components/ForgetPassword/ResetPassword.jsx
+++ src/Components/ForgetPassword/ResetPassword.jsx
@@ -3,7 +3,7 @@ import { useRef, useContext,useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams, useNavigate } from 'react-router-dom'
 import "./ForgetPassword.css"
-// import axios from 'axios'
+import axios from 'axios'
 const ResetPassword = () => {
     const user = useSelector((state) => state.Commerce.user)
     const navigate = useNavigate()
@@ -14,7 +14,7 @@ const ResetPassword = () => {
     const ResetPass = async () => {
         console.log("clicked")
         const res = await axios.post(`https://eduglobal.onrender.com/api/admin/resetPassword/${id}`,)
-        res.status === 200 ? navigate('/loginuser/login') : null
+        res.status === 201 ? navigate('/loginuser/login') : null
         console.log(res)
       }
 
