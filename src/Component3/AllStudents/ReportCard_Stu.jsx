89420f8f45edaefad1340957c64a0454e6e31375
youngmentor <olaleresuliton43@gmail.com>
2023-03-06 18:42:03 -0800

    handle upload function

--- src/Component3/AllStudents/ReportCard_Stu.jsx
+++ src/Component3/AllStudents/ReportCard_Stu.jsx
@@ -2,20 +2,21 @@ import React, { useState } from 'react'
 import { useParams } from 'react-router-dom';
 import Loading from '../../Components/LoadingSpin/Loading'
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 const ReportCard_Stu = () => {
+  const teacher = useSelector((state) => state.Commerce.teacher)
   const { id } = useParams()
   const [load, setLoad] = useState(false)
   const [message, setMessage] = useState('');
   const [profile, setProfile]= useState({
-    currentSchoolTerm:  "",
-	resultImage:  ""
+    currentSchoolTerm: "",
+	resultImage:""
   })
   const handleImage = (e) => {
     const file = e.target.files[0];
     setProfile({ ...profile, resultImage: file });
   };
   const handleChange = (e) => {
-    setProfile({ ...profile, [e.target.name]: e.target.value });
   };
   const handleFileUpload = (e) => {
     e.preventDefault();
@@ -23,14 +24,15 @@ const ReportCard_Stu = () => {
     const formData = new FormData();
     formData.append('currentSchoolTerm', profile.currentSchoolTerm);
     formData.append('resultImage', profile.resultImage);
-
-    axios.post(`https://eduglobal.onrender.com/api/teacher/result/${teacher._id}${id}`, formData, {
+      //  console.log(profile)
+    axios.post(`https://eduglobal.onrender.com/api/teacher/result/${teacher._id}/${id}`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data'
       }
     })
       .then(response => {
         setMessage(response.data.message);
+        setLoad(false)
       })
       .catch(error => {
         console.error(error);
@@ -41,7 +43,9 @@ const ReportCard_Stu = () => {
     <div className='ReportCardPage'>
       <form onSubmit={handleFileUpload} className="ReportForm">
         <p>Upload Result</p>
-        <input type="text" placeholder='School term' className='ResultInput' onChange={handleChange} />
+        <input type="text" placeholder='School term' name="currentSchoolTerm" className='ResultInput' onChange={(e)=>{  setProfile({ ...profile, currentSchoolTerm: e.target.value })  }} />
+
+
        <div className="ReportFile"> <input type="file" placeholder='choose a file'  onChange={handleImage}  /></div>
         <button onClick={handleFileUpload} className="ReportBttn">{load ? <Loading /> : "Upload"}</button>
         {message && <p>{message}</p>}
